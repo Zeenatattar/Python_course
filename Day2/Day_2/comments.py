bc4c9fe909de98275c97c87950a385fece88a7c0
Zeenat <111846814+Zeenatattar@users.noreply.github.com>
2025-07-22 19:01:47 +0530

    Add files via upload

---
+++ Day2/Day_2/comments.py
@@ -0,0 +1,6 @@
+def print_info():
+    name = "zeenat ilahi attar"    #storing name
+    age = "21"                     #storing age
+    print("My name is:",name)      #printing name
+    print("my age is:",age)        #printing age
+print_info()                       #calling function
